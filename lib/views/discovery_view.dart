d0988b6936c4010253bf75af7091040ca93e1a89
AbdelazizSameh <abdelzizsameh364@gmail.com>
2025-11-04 11:38:23 +0200

    add discovert view

---
+++ lib/views/discovery_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DiscoveryView extends StatelessWidget {
+  const DiscoveryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
